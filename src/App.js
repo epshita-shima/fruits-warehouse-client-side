d4813f464ffd613d7129f6989b9f84ca5dbd065d
epshita-shima <epshitashima@gmail.com>
2022-05-07 17:39:24 +0600

    complete footer section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Header from './pages/Shared/Header/Header';
 import Home from './pages/Home/Home/Home';
+import Footer from './pages/Shared/Footer/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<Home></Home>} ></Route>
         <Route path='/home' element={<Home></Home>}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -1,10 +1,59 @@
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
+    const today = new Date();
+    const year = today.getFullYear()
     return (
         <div>
+            <div className='footer-bg p-5 text-white' >
+                <div className="row">
+                    <div className="col-md-3">
+                        <img className='img-fluid w-50 mb-3' src="https://cdn.shopify.com/s/files/1/0309/2968/2571/files/Obrien-Logo_125x@2x.png?v=1595923730" alt="" />
+                        <p className='text-white'>Fruits are an excellent source of essential vitamins and minerals, and they are high in fiber. Fruits also provide a wide range of health-boosting antioxidants, including flavonoids.</p>
 
+                    </div>
+                    <div className="col-md-3">
+                        <div className='footer-child'>
+                            <h2 className='text-center'>Information</h2>
+                            <ul>
+                                <li>Our company</li>
+                                <li>Contact Us</li>
+                                <li>Our services</li>
+                                <li>Why we?</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-md-3">
+                        <div className='footer-child'>
+                            <h2 >Quicklink</h2>
+                            <ul>
+                                <li>About</li>
+                                <li>Blog</li>
+                                <li>Shop</li>
+                                <li>Conatct</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-md-3">
+
+                        <div className='footer-child'>
+                            <h2 className='text-center'>Support</h2>
+                            <ul>
+                                <li>Online support</li>
+                                <li>Shipping policy</li>
+                                <li>Privecy policy</li>
+                                <li>Terms of service</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='footer-bottom p-3 text-white'>
+                <p>copyright &copy; {year}</p>
+            </div>
         </div>
+
     );
 };
 
