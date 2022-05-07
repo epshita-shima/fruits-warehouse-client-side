19d32eb39384489100af6f51ada622853b30b781
epshita-shima <epshitashima@gmail.com>
2022-05-07 07:31:35 +0600

    header complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from './pages/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-
+      <Header></Header>
+      <Routes>
+        <Route path=''></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Shared/Header/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <>
+            <Navbar collapseOnSelect expand="lg" sticky='top' className='header-background' variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home"><img className='w-50' src="https://shopdemo.hasthemes.com/p2/img/obrien/logo.png" alt="" /></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                        </Nav>
+                        <Nav>
+                            <Nav.Link as={Link} to="home">Home</Nav.Link>
+                            <Nav.Link href="home#services">Inventory</Nav.Link>
+                            <Nav.Link as={Link} to="about">About</Nav.Link>
+                            <Nav.Link as={Link} to="blogs">Blogs</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+};
+
+export default Header;
